5f313a3a984c2bd5cf32d01961cac67a9b9ec331
jeff0brewer <jeff0brewer@gmail.com>
2022-06-17 10:51:43 -0700

    gl lib added, display layout init

--- pages/comp/display.js
+++ pages/comp/display.js
@@ -7,14 +7,21 @@ const Display = props => {
     const errLogRef = useRef(null)
     const frameIdRef = useRef(null)
 
-    let gl, vertSource, glBuffer, aPosition;
+    let gl, vertSource, fragLib, glBuffer, aPosition, uDimensions, uTime;
     const glu = new GlUtil()
     const buffer = new Float32Array([-1, -1, -1, 1, 1, -1, 1, 1])
     const fsize = buffer.BYTES_PER_ELEMENT
     const fragTEST_ONLY = `
-        precision highp float;
+        uniform vec2 uDimensions;
+        uniform float uTime;
         void main() {
-            gl_FragColor = vec4(1.0, 0.3, 1.0, 1.0);
+            vec3 coord = vec3(gl_FragCoord.xy/uDimensions, uTime);
+
+            vec3 col = vec3(0.0, 0.0, 0.0);
+            col.x += noise(coord, 1.0);
+            col.y += noise(coord, 1.0, vec2(-1.0, 1.0));
+            col.z += noise(coord, 1.0, vec2(1.0, -1.0));
+            gl_FragColor = vec4(col, 1.0);
         }
     `
 
@@ -27,6 +34,8 @@ const Display = props => {
             requestFrame(draw)
             return
         }
+
+        gl.uniform1f(uTime, time*.001)
         gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT)
         gl.bindBuffer(gl.ARRAY_BUFFER, glBuffer)
         gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 2 * fsize, 0)
@@ -37,9 +46,16 @@ const Display = props => {
 
     const initGl = async () => {
         vertSource = await (await fetch('./vert.glsl')).text()
-        gl = glu.setupGl(canvRef.current, [vertSource, fragTEST_ONLY])
+        fragLib = await( await fetch('./fragLib.glsl')).text()
+
+        gl = glu.setupGl(canvRef.current, [vertSource, fragLib + fragTEST_ONLY])
         glBuffer = glu.initBuffer(buffer, gl.STATIC_DRAW)
         aPosition = glu.initAttribute('aPosition', 2, 2, 0, fsize)
+
+        uDimensions = gl.getUniformLocation(gl.program, 'uDimensions')
+        gl.uniform2f(uDimensions, props.width, props.height)
+        uTime = gl.getUniformLocation(gl.program, 'uTime')
+        gl.uniform1f(uTime, 0)
     }
 
     useEffect(() => {
@@ -54,13 +70,19 @@ const Display = props => {
     return (
         <section className={styles.display}>
             <div>
-                <canvas ref={canvRef} width={props.width} height={props.height}></canvas>
-                <p ref={errLogRef}></p>
+                <div className={styles.view}>
+                    <canvas ref={canvRef} width={props.width} height={props.height}></canvas>
+                    <p ref={errLogRef}></p>
+                </div>
+                <div className={styles.controls}>
+                    <span> 
+                        <input type="text" value={props.width}/> 
+                        x
+                        <input type="text" value={props.height}/> 
+                    </span>
+                    <button>fullscreen</button>
+                </div>      
             </div>
-            <div>
-                <input type="text" />
-                <button></button>
-            </div>      
         </section>
     )
 }
