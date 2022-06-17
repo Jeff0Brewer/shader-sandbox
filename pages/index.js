c543ce0620cccafc36cbd573ee8df7a2a6970e04
jeff0brewer <jeff0brewer@gmail.com>
2022-06-17 11:56:08 -0700

    init code textbox

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
+import React, { useState, useEffect } from 'react'
 import Display from './comp/display.js'
+import Code from './comp/code.js'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
-  return (
-      <main className={styles.home}>
-        <Display width={800} height={700} />
-      </main>
-  )
+    const [code, setCode] = useState(null)
+    const [defCode, setDefCode] = useState(null)
+
+    const getDefaultCode = async () => {
+        const res = await (await fetch('./fragDefault.glsl')).text()
+        setDefCode(res)
+        setCode(res)
+    }
+    useEffect(() => {
+        getDefaultCode()
+    }, [])
+
+    return (
+        <main className={styles.home}>
+            <Code setCode={setCode} defaultCode={defCode}/>
+            <Display width={800} height={700} code={code} />
+        </main>
+    )
 }
 
 export default Home
